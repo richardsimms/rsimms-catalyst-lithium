5870c76ddae4364e69b64d3781dac84f99b62f96
Richard Simms <simmsdesign@gmail.com>
2022-05-13 22:42:25 +1000

    Small updates

--- src/gatsby-theme-catalyst-blog/utils/use-catalyst-blog-config.js
+++ src/gatsby-theme-catalyst-blog/utils/use-catalyst-blog-config.js
@@ -4,7 +4,7 @@ export const useCatalystBlogConfig = () => {
     graphql`
       query {
         
-        catalystBlogConfig {
+        catalystBlogConfig  {
           postListTitle
           displayPostListTitle
       
